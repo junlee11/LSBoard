8c97946a7f2192c34eb3666074e9b8cd35059acf
junlee11 <ijungyoung@naver.com>
2023-05-29 15:59:09 +0900

    230529 15:59

--- LSBoard_PJT/main/views.py
+++ LSBoard_PJT/main/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import getpass
 
 # Create your views here.
 def index(request):
     #return HttpResponse("Main Screen!!!")
-    return render(request, 'index.html')
+    return render(request, 'main/index.html')
+
+#로그인창 처리
+def LoginResponse(request):
+    username = getpass.getuser()
+    print(username)
+    
+    if username == 'heeyoung':
+        return render(request, "category/index.html")
+    else:
+        return HttpResponse("권한이 없습니다.")
---
+++ LSBoard_PJT/static/js/category/category.js
@@ -0,0 +1,7 @@
+$("#menu-toggle").click(function(e) {
+    alert("Warning Message");
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+  });
+  
+  
